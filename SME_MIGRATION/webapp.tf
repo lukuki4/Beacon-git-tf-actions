b38b380dd44c3b217fe7ff54ce4838cf2ce74c28
Your Name <you@example.com>
2022-08-01 15:02:29 +0000

    ggf

--- SME_MIGRATION/webapp.tf
+++ SME_MIGRATION/webapp.tf
@@ -16,6 +16,13 @@ resource "azurerm_app_service" "beaconsmeinc" {
   app_service_plan_id = azurerm_app_service_plan.becsme.id  
   https_only = false
 
+resource "azurerm_application_insights" "beaconsmeinc" {
+  name                = "beaconweb-appinsights"
+  location            = azurerm_resource_group.beaconsme_gp.location
+  resource_group_name = azurerm_resource_group.beaconsme_gp.name
+  application_type    = "web"
+}
+
   site_config {
     dotnet_framework_version = "v6.0"
     always_on                = false
