c6e58e36dd0290db9682601f7e086e51e47b2e06
Your Name <you@example.com>
2022-09-21 13:44:49 +0000

    beacon

--- SME_MIGRATION/main.tf
+++ SME_MIGRATION/main.tf
@@ -143,5 +143,5 @@ resource "azurerm_virtual_machine" "main" {
   location            = azurerm_resource_group.beaconsmes_gp.location
   resource_group_name = azurerm_resource_group.beaconsmes_gp.name
   platform_fault_domain_count = 1
-  platform_update_domain_count = 2
+  platform_update_domain_count = 1
   }
